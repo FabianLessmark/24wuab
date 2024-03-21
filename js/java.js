3f9af5e4d3531d17558562914cc40bdf5d5d0db4
FabianLessmark <fabian.lessmark@elev.ga.ntig.se>
2024-03-21 16:06:35 +0100

    update 7

--- js/java.js
+++ js/java.js
@@ -11,3 +11,13 @@ function showMenu(){
     let layer = document.querySelector(".fade-layer")
     layer.classList.toggle("visible")
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+        
+    document.getElementById('response').innerHTML = '<p>Tack så mycket, ' + name + ', ditt meddelande har skickats! Vi hör av oss så snart som möjligt!</p>';
+});
